5452621306da8920d86da1664ea61c35b9b906fb
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-02-19 14:05:23 -0300

    criando superclasse produto

--- AtividadePratica02/entidades/Produto.cs
+++ AtividadePratica02/entidades/Produto.cs
@@ -1,4 +1,71 @@
 namespace Entidades
 {
+    public abstract class Produto
+    {
+        public int Codigo {get ; set ;}
+        public string Nome {get ; set ;}
+        public double Preco {get ; set ;}
+        public string Descricao {get ; set ;}
+
+        public int ValidaCodigo (int codigo)
+        {
+            if (codigo < 0)
+            {
+                throw new System.Exception("Código inválido.");
+            }
+
+            else
+            {
+                return codigo;
+            }
+        }
+
+        public string ValidaNome (string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome)) 
+            {
+                throw new System.Exception("Nome inválido.");
+            }
+
+            else
+            {
+                return nome;
+            }
+        }
     
+        public double ValidPreco (double preco)
+        {
+            if (preco < 0)
+            {
+                throw new System.Exception("Preço inválido.");
+            }
+
+            else
+            {
+                return preco;
+            }
+        }
+    
+        public string ValidaDescricao (string descricao)
+        {
+            if (string.IsNullOrWhiteSpace(descricao))
+            {
+                throw new System.Exception("Descrição inválida.");
+            }
+
+            else
+            {
+                return descricao;
+            }
+        }
+    
+        public Produto (int codigo, string nome, double preco, string descricao)
+        {
+            Codigo = ValidaCodigo(codigo);
+            Nome = ValidaNome(nome);
+            Preco = ValidPreco(preco);
+            Descricao = ValidaDescricao(descricao);
+        }
+    
+    }
 }
