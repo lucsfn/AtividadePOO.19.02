1bf0eb1b2bc4670864a8f02fd46fa416da927a88
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-02-19 16:23:57 -0300

    implementacao subclasse musica

---
+++ AtividadePratica02/entidades/derivados/Musica.cs
@@ -0,0 +1,65 @@
+using Entidades;
+
+namespace Derivados
+{
+    public class Musica : Midia
+    {
+        public string Autor { get; set; }
+        public string GeneroMusical { get; set; }
+        public string Letra { get; set; }
+        public TimeOnly Duracao { get; set; }
+
+        public string ValidaAutor(string autor)
+        {
+            if (string.IsNullOrWhiteSpace(autor))
+            {
+                throw new System.Exception("Autor inválido.");
+            }
+
+            else
+            {
+                return autor;
+            }
+        }
+
+        public string ValidaGeneroMusical(string generoMusical)
+        {
+            if (string.IsNullOrWhiteSpace(generoMusical))
+            {
+                throw new System.Exception("Gênero inválido.");
+            }
+
+            else
+            {
+                return generoMusical;
+            }
+        }
+
+        public string ValidaLetra(string letra)
+        {
+            if (string.IsNullOrWhiteSpace(letra))
+            {
+                throw new System.Exception("Letra inválida.");
+            }
+
+            else
+            {
+                return letra;
+            }
+        }
+
+        public Musica (string nome, DateTime dataDeLancamento, string idioma, string autor, string generoMusical, string Letra, TimeOnly duracao) : base (nome, dataDeLancamento, idioma)
+        {
+            Autor = ValidaAutor(autor);
+            GeneroMusical = ValidaGeneroMusical(generoMusical);
+            Letra = ValidaLetra(Letra);
+            Duracao = duracao;
+        }
+
+        public override string ListaMidia()
+        {
+            return $"Nome: {Nome}\nData de lançamento: {DataDeLancamento}\nIdioma: {Idioma}\nAutor: {Autor}\nGênero musical: {GeneroMusical}\nLetra: {Letra}\nDuração: {Duracao}";
+        }
+
+    }
+}
--- AtividadePratica02/obj/Debug/net8.0/AtividadePratica02.AssemblyInfo.cs
+++ AtividadePratica02/obj/Debug/net8.0/AtividadePratica02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadePratica02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3806c90c21da3d7de8486fd830ed305da28d98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6bb083e0c6c8a99312ee2720e85afee44002e25")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadePratica02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadePratica02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
