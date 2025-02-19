841c7009f3e771bd2ff42aef39bb69da29018b89
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-02-19 16:02:47 -0300

    implementado superclasse midia

--- AtividadePratica02/entidades/Midia.cs
+++ AtividadePratica02/entidades/Midia.cs
@@ -1,4 +1,61 @@
 namespace Entidades
 {
-    
+    public abstract class Midia
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public DateTime DataDeLancamento { get; set; }
+
+        public string ValidaNome(string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new System.Exception("Nome inválido.");
+            }
+
+            else
+            {
+                return nome;
+            }
+        }
+
+        public string ValidaAutor(string autor)
+        {
+            if (string.IsNullOrWhiteSpace(autor))
+            {
+                throw new System.Exception("Autor inválido.");
+            }
+
+            else
+            {
+                return autor;
+            }
+        }
+
+        public DateTime ValidaDataDeLancamento(DateTime dataDeLancamento)
+        {
+            if (dataDeLancamento > DateTime.Now)
+            {
+                throw new System.Exception("Data de lançamento inválida.");
+            }
+
+            else
+            {
+                return dataDeLancamento;
+            }
+        }
+
+        public Midia(string nome, string autor, DateTime dataDeLancamento)
+        {
+            Nome = ValidaNome(nome);
+            Autor = ValidaAutor(autor);
+            DataDeLancamento = ValidaDataDeLancamento(dataDeLancamento);
+        }
+
+        public virtual string ListaMidia ()
+        {
+            return $"Nome: {Nome}\nAutor: {Autor}\nData de lançamento: {DataDeLancamento}";
+        }
+
+    }
 }
